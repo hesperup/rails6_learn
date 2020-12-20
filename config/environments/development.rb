903e8e144fc06069ec1fd63d19af07c394d5ee86
hesper <hesperup@gmail.com>
2020-12-20 11:08:15 +0800

    email and job

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,10 +29,21 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sina.com",
+    port:  465,
+    ssl:   true,
+    authentication: "plain",
+    user_name: "ces@sina.com",
+    password: "ces",
+    enable_starttls_auto: true
+    }
 config.action_cable.disable_request_forgery_protection = true
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
